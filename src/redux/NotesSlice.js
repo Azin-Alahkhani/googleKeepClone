ebb7ea2ebd69f67a561c1e057ef5fd6c7bc32e64
azin_alahkhani <aalahkhani@gmail.com>
2025-04-07 14:20:46 +0100

    fix bgColor bug

--- src/redux/NotesSlice.js
+++ src/redux/NotesSlice.js
@@ -2,8 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   notes: [
-    { id: 1, title: "First Note", content: "This is my first note. Lets make it a tad bit longer to check the ui under different conditions", labels: ["Work"] ,bgColor:"#202124",img:""},
-    { id: 2, title: "Second Note", content: "Google Keep Clone! lets see how close we can get to the actual design. seems easy but we'll have to wait and see.", labels: ["Personal"],bgColor:"#202124",img:"" },
+    {
+      id: 1,
+      title: "First Note",
+      content:
+        "This is my first note. Lets make it a tad bit longer to check the ui under different conditions",
+      labels: ["Work"],
+      bgColor: "#202124",
+      img: "",
+    },
+    {
+      id: 2,
+      title: "Second Note",
+      content:
+        "Google Keep Clone! lets see how close we can get to the actual design. seems easy but we'll have to wait and see.",
+      labels: ["Personal"],
+      bgColor: "#202124",
+      img: "",
+    },
   ],
   selectedLabel: null, // For filtering
 };
@@ -16,15 +32,16 @@ const notesSlice = createSlice({
       state.notes.push(action.payload);
     },
     removeNote: (state, action) => {
-      state.notes = state.notes.filter(note => note.id !== action.payload);
+      state.notes = state.notes.filter((note) => note.id !== action.payload);
     },
     editNote: (state, action) => {
-      const { id, title, content, labels , img } = action.payload;
-      const note = state.notes.find(note => note.id === id);
+      const { id, title, content, labels, img } = action.payload;
+      const note = state.notes.find((note) => note.id === id);
       if (note) {
         note.title = title;
         note.content = content;
         note.labels = labels;
+        note.bgColor = action.payload.bgColor || note.bgColor;
         note.img = img;
       }
     },
@@ -34,5 +51,6 @@ const notesSlice = createSlice({
   },
 });
 
-export const { addNote, removeNote, editNote, setSelectedLabel } = notesSlice.actions;
+export const { addNote, removeNote, editNote, setSelectedLabel } =
+  notesSlice.actions;
 export default notesSlice.reducer;
